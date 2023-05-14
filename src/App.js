56de018f0d57472768a8d80aacc77fb8e22d7238
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-14 20:29:43 +0530

    docs: update app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import "./App.css";
+import { Route, Routes } from 'react-router';
+import './App.css';
+import { Home } from './pages/home/home';
+import { Product } from './pages/product/product';
+import { NavBar } from './components/navbar/navbar';
+import { Cart } from './pages/cart/cart';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
     </div>
   );
 }
