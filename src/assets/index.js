0e861e5e33f40a7a42ab02bcf2142abac88c1a42
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-15 00:52:31 +0530

    docs: add assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -14,4 +14,23 @@ const dresses = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v168389
 const ethnic = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683898986/ethnic_llwxrg.jpg';
 const cartEmpty = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684057227/free-empty-cart-4085814-3385483_c7dwif.webp';
 
+const top1 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684087983/d7ae3065-98d4-42d0-b244-d2b18802ff101601959656577-SASSAFRAS-Women-Black-Solid-High-Neck-Cropped-Top-4441601959-1_vyybyh.jpg'
+const top2 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684088084/df0c1d8c-528d-4afd-9714-a40c19b105ec1531903512284-Pannkh-Women-Navy-Blue-Printed-Top-6761531903512079-1_mgfh4v.jpg';
+const top3 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684088885/67aab8fc-48a5-4c48-8d73-108ff7b458a81614680959709-1_ylbawd.jpg';
+const top4 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684089088/fc81590e-5820-4f89-b687-208b31112f641632917380502-DressBerry-Burgundy-Hooded-Solid-Blouson-Crop-Top-7891632917-1_qdfe6y.jpg';
+const top5 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684089331/899b523a-f57a-48fb-80cd-8422614f195d1580451588210-SASSAFRAS-Women-Green--Beige-Printed-Blouson-Top-49515804515-1_kqerz2.jpg';
+const top6 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684089561/82c99b23-53ea-492c-8b46-26d65804125f1670934663294-DressBerry-White--Yellow-Floral-Print-Top-2181670934662918-1_rzsmt1.jpg'
+const top7 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684089751/00bbdd98b4204974b41d030b1f289130UR_w1000_q90_yf2cpx.webp';
+const top8 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684089918/2b31239621e74af895eaf9d8b053c7ec_w1000_q90_yh5tjt.webp'; 
+const bottom1 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684090099/71c365f84627453d8c696e2064fcc9fc_w1000_q90_pobdpz.webp';
+const bottom2 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684090339/066aec8e-c4f6-480e-b7d3-079f84b026151660906310440AllenSollyBlackTrousers1_fg7fou.jpg';
+const bottom3 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684090809/bacb67b9-c690-4ae5-9172-b3db6bae8aac1675772974887Widetwilltrousers1_nf8etj.jpg';
+const bottom4 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684090984/35cace73-95ce-4140-b0d9-ecad905b07c71663318751220-SASSAFRAS-Women-Track-Pants-6011663318750539-2_wznwix.jpg';
+const bottom5 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684091197/5b6f5d45769447ec9c064cde78a2655fUR_w1000_q90_skjrbg.webp';
+const bottom6 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684091503/8be830fa-f64d-42e2-aba3-3e5cf9543a311652423912513-Levis-Women-Olive-Green-Camouflage-Printed-Joggers-366165242-1_io1wn1.jpg';
+const botttom7 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684091725/52e37168-d484-47c9-8dbe-e4343b81ff101670320930434-DressBerry-Women-Solid-Track-Pants-7231670320929805-1_bjfvjy.jpg';
+const dress1 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684091889/fcdd1e3b39bd401bacebb8534ca9d896_w1000_q90_tymves.webp';
+const dress2 = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684092039/1b9df8fd-214e-4e8c-a0b6-759a9167e1e61564379559418-SASSAFRAS-Women-Black-A-Line-Dress-291564379558115-1_atp5ur.jpg';
+
 export {logo, header, hm, louis, gucci, levis, zara, dolce, prada, calvin, tops, bottom, dresses, ethnic, cartEmpty};
+export {top1, top2, top3, top4, top5, top6, top7, top8, bottom1, bottom2, bottom3, bottom4, bottom5, bottom6, botttom7, dress1, dress2}
