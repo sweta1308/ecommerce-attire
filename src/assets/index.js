c872eb717e37b58c12a283eb5d41134599f64605
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-14 20:12:57 +0530

    docs: update assets

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+const logo = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683874845/logo.png';
+const header = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683881822/hero-img.jpg';
+const hm = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683887347/h_m_q8hrlz.png';
+const louis = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683887304/louis_wr6fze.png';
+const gucci = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683887356/gucci_gkxrjf.png';
+const levis = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683887309/levis_uhbuun.png';
+const zara = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683887744/UkabSwd9CX7buvuAztJG8J-320-80_m4lu48.jpg';
+const dolce = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683887321/dolce_msbiub.png';
+const prada = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683887329/prada_e1ikmw.jpg';
+const calvin = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683893328/calvin_aosng2.png';
+const tops = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683898962/top_kokptn.jpg';
+const bottom = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683918962/download_oqwglc.jpg';
+const dresses = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683898977/dresses_wbk7k5.jpg';
+const ethnic = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683898986/ethnic_llwxrg.jpg';
+const cartEmpty = 'https://res.cloudinary.com/sweta-agarwalla/image/upload/v1684057227/free-empty-cart-4085814-3385483_c7dwif.webp';
+
+export {logo, header, hm, louis, gucci, levis, zara, dolce, prada, calvin, tops, bottom, dresses, ethnic, cartEmpty};
