af5ff42fbbf87b0b0542e76d2f1f788ef289a3eb
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-15 23:51:29 +0530

    style: update css

--- src/components/product-card/productCard.jsx
+++ src/components/product-card/productCard.jsx
@@ -4,16 +4,6 @@ export const ProductCard = ({image, title, brand, price, originalPrice, ratings}
     return (
         <>
             <div className="product-card">
-                {/* <img src={data.image} alt={data.title} />
-                <div className="first-line">
-                    <h3>{data.brand}</h3>
-                    <h3>₹{data.price}</h3>
-                </div>
-                <p>{data.title}</p>
-                <div className="second-line">
-                    <p>{data.ratings.value} ⭐</p>
-                    <p>₹{data.originalPrice}</p>
-                </div>  */}
                 <img src={image} alt={title} />
                 <h3>{brand}</h3>
                 <p className='product-title'>{title}</p>
