3f126ca1b9b5a163d1ee088d8a8347d0ab06e227
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-16 17:28:40 +0530

    docs: update login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,7 +1,47 @@
+import './login.css';
+
 export const Login = () => {
     return (
-        <>
-            <h1>Login</h1>
-        </>
+        <div>
+
+            {/* <div className="login">
+                <h1>Login</h1>
+                <div className='name'>
+                    <div>
+                        <label for='first-name'>First name: </label>
+                        <input id='first-name' placeholder='John' />
+                    </div>
+                    <div>
+                        <label for='last-name'>Last name: </label>
+                        <input id='last-name' placeholder='Doe' />
+                    </div>
+                </div>
+                
+                <div>
+                    <label for='email'>Email:</label>
+                    <input id='email' placeholder='johndoe@example.com' />
+                </div>
+                
+            </div> */}
+
+            <div className='login'>
+                <h2>Login</h2>
+                <div>
+                    <label for='email'>Email:</label>
+                    <input id='email' placeholder='johndoe@example.com' />
+                </div>
+
+                <div>
+                    <label for='password'>Password:</label>
+                    <input id='password' type='password' placeholder='********' />
+                </div>
+
+                <button className='login-button'>Login</button>
+                <button className='login-button guest'>Login As Guest</button>  
+
+                <p>Create New account <i class="fa-solid fa-angle-right"></i></p>
+            </div>
+        
+        </div>
     )
 }
