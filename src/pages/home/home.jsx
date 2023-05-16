e63fa512fd901421f29a1f16c3ad553547efd157
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-16 21:22:32 +0530

    docs: update footer

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,13 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { Brand } from "../../components/brand/brand";
 import { Featured } from "../../components/featured/featured";
 import { Category } from "../../components/category/category";
-import { header } from "../../assets";
+import { header, logo } from "../../assets";
 import './home.css';
+// import { Footer } from "../../components/footer/footer";
 
 export const Home = () => {
+    const navigate = useNavigate();
     return (
         <>
             <div className="hero">
@@ -50,16 +52,29 @@ export const Home = () => {
                 </div>
                 <Featured />
                 <Category />
-                
-
             </div>
-
             <footer>
                 <div className="footer-div">
-                    <h4>CONTACTS: </h4>
-                    <a href="https://github.com/sweta1308"><i class="fa-brands fa-github fa-lg"></i></a>
-                    <a href="https://twitter.com/AgarwallaSweta"><i class="fa-brands fa-twitter fa-lg"></i></a>
-                    <a href="https://www.linkedin.com/in/sweta-agarwalla-45aa2324a/"><i class="fa-brands fa-linkedin fa-lg"></i></a>
+                    <div className="logo-footer">
+                        <img src={logo} alt="" />
+                        <p>Specializes in providing high-quality, stylish productsfor your wardrobe</p>
+                    </div>
+                    <div className="contacts"> 
+                        <div className="companies">
+                            <h3>COMPANY</h3>
+                            <p onClick={() => navigate('/')}>About</p>
+                            <p onClick={() => navigate('/')}>Terms of Use</p>
+                            <p onClick={() => navigate('/')}>Policies</p>
+                        </div>
+
+                        <div className="socials">
+                            <h3>CONTACTS: </h3>
+                            <a href="https://github.com/sweta1308"><i class="fa-brands fa-github fa-lg"></i> Github</a>
+                            <a href="https://twitter.com/AgarwallaSweta"><i class="fa-brands fa-twitter fa-lg"></i> Twitter</a>
+                            <a href="https://www.linkedin.com/in/sweta-agarwalla-45aa2324a/"><i class="fa-brands fa-linkedin fa-lg"></i> LinkedIn</a>
+                        </div>
+                    </div>
+                    
                 </div>
                 <hr />
                 <p>Copyright <i class="fa-regular fa-copyright"></i>2023 Attire. All rights reserved.</p>
