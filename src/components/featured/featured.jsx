2f22d580d22773d6a0838cef79e18cfdffd2c05b
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-16 16:12:33 +0530

    update changes in component

--- src/components/featured/featured.jsx
+++ src/components/featured/featured.jsx
@@ -1,8 +1,20 @@
+import { featuredData } from "./featuredData";
+import { ProductCard } from "../product-card/productCard";
+import './featured.css'
+
 export const Featured = () => {
     return (
         <>
             <h2>Products You May Like ✨</h2>
-            
+            <div className="featured">
+                {featuredData.map(data => {
+                    const {_id, title, brand, price, originalPrice, ratings, image} = data;
+                    return (
+                    <div key={_id}>
+                        <ProductCard title={title} brand={brand} price={price} originalPrice={originalPrice} image={image} ratings={ratings} />
+                    </div>
+                )})}
+            </div>
         </>
     )
 }
