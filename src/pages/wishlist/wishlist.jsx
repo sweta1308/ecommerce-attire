a47655b34f4e028d6bfd1d49592ada00ca746268
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-16 16:54:05 +0530

    docs: update empty wishlist content

--- src/pages/wishlist/wishlist.jsx
+++ src/pages/wishlist/wishlist.jsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router";
+import './wishlist.css';
+
 export const Wishlist = () => {
+    const navigate = useNavigate();
     return (
         <>
-            <h1>Wishlist</h1>
+            <div className="wishlist">
+                <h1>Wishlist</h1>
+                <div className='wishlist-items'>
+                    <h3>Nothing in wishlist.</h3>
+                    <p>Browse through products and add your favourites to wishlist.</p>
+                    <button onClick={()=> navigate('/products')}>Browse</button>
+                </div>
+            </div>
         </>
     )
 }
