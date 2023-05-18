60de3d1d616288b3915b8b8bcc2dca356429e7b0
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-18 15:27:35 +0530

    refactor: update password field

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -1,8 +1,15 @@
 import { useNavigate } from 'react-router';
 import './signup.css';
+import { useState } from 'react';
 
 export const Signup = () => {
     const navigate = useNavigate();
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
+    const handlePasswordClick = () => setIsPasswordVisible(prev => !prev)
+    const handleConfirmPasswordClick = () => setIsConfirmPasswordVisible(prev => !prev)
+
+    
     return (
         <>
             <div className="signup">
@@ -25,15 +32,21 @@ export const Signup = () => {
                 
                 <div>
                     <label for='password'>Password: </label>
-                    <input id='password' type='password' placeholder='********' />
+                    <div className='password-wrapper'>
+                        <input id='password' type={isPasswordVisible ? 'text' : 'password'} placeholder={isPasswordVisible ? 'password' : '********'} /> 
+                        <button onClick={handlePasswordClick}>{isPasswordVisible ? <i class="fa-regular fa-eye-slash"></i> : <i class="fa-regular fa-eye"></i>}</button>
+                    </div>
                 </div>
 
                 <div>
                     <label for='confirm-password'>Confirm Password: </label>
-                    <input id='confirm-password' type='password' placeholder='********' />
+                    <div className='password-wrapper'>
+                        <input id='password' type={isConfirmPasswordVisible ? 'text' : 'password'} placeholder={isConfirmPasswordVisible ? 'password' : '********'} /> 
+                        <button onClick={handleConfirmPasswordClick}>{isConfirmPasswordVisible ? <i class="fa-regular fa-eye-slash"></i> : <i class="fa-regular fa-eye"></i>}</button>
+                    </div>
                 </div>
 
-                <button>Signup</button>
+                <button className='button-signup'>Signup</button>
 
                 <p onClick={() => navigate('/login')}>Login to existing account <i class="fa-solid fa-angle-right"></i></p>
             </div>
