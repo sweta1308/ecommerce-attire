d82cdda1915f8806cf1e60ad7901b0146c9dd0d3
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-18 18:09:29 +0530

    feat: update login features

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,35 @@
 import { useNavigate } from 'react-router';
 import './login.css';
 import { useState } from 'react';
+import { useAuth } from '../../context/authContext';
 
 export const Login = () => {
     const navigate = useNavigate();
+    const {userLogin} = useAuth();
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-    const handlePasswordClick = () => setIsPasswordVisible(prev => !prev)
+    const [userData, setUserData] = useState({
+        email: '',
+        password: ''
+    })
+    const testUserData = {
+        email: 'johndoe@gmail.com',
+        password: 'john1234'
+    }
+
+    const handlePasswordClick = () => setIsPasswordVisible(prev => !prev);
+
+    const handleLogin = () => {
+        if (!userData.email.trim() || !userData.password.trim()) {
+            alert('Enter proper credentials.')
+        } else {
+            userLogin(userData)
+        }
+    }
+
+    const handleTestLogin = () => {
+        setUserData(testUserData);
+        userLogin(testUserData)
+    }
 
     return (
         <div>
@@ -13,19 +37,19 @@ export const Login = () => {
                 <h2>Login</h2>
                 <div>
                     <label for='email'>Email:</label>
-                    <input id='email' placeholder='johndoe@example.com' />
+                    <input id='email' placeholder='johndoe@example.com' value={userData.email} onChange={(e) => setUserData(prev => ({...prev, email: e.target.value}))} />
                 </div>
 
                 <div>
                     <label for='password'>Password:</label>
                     <div className='password-wrapper'>
-                        <input id='password' type={isPasswordVisible ? 'text' : 'password'} placeholder={isPasswordVisible ? 'password' : '********'} /> 
+                        <input id='password' type={isPasswordVisible ? 'text' : 'password'} placeholder={isPasswordVisible ? 'password' : '********'} value={userData.password} onChange={(e) => setUserData(prev => ({...prev, password: e.target.value}))} /> 
                         <button onClick={handlePasswordClick}>{isPasswordVisible ? <i class="fa-regular fa-eye-slash"></i> : <i class="fa-regular fa-eye"></i>}</button>
                     </div>
                 </div>
 
-                <button className='login-button'>Login</button>
-                <button className='login-button guest'>Login As Guest</button>  
+                <button className='login-button' onClick={handleLogin}>Login</button>
+                <button className='login-button guest' onClick={handleTestLogin}>Login As Guest</button>  
 
                 <p onClick={() => navigate('/signup')}>Create New account <i class="fa-solid fa-angle-right"></i></p>
             </div>
