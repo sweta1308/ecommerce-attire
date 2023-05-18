eb38e8ae16c8e6c77bf5f23b7f4f5eb2737670e0
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-18 15:50:07 +0530

    feat: update login user function

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,11 +1,42 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import { authReducer } from "../reducer/authReducer";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => { 
+    const authInitial = {
+        isLoggedIn: false,
+        user: {},
+        token: '',
+    }
+    const [authState, authDispatch] = useReducer(authReducer, authInitial);
+    const navigate = useNavigate();
+    // const location = useLocation()
+
+    const userLogin = async (loginData) => {
+        try {
+            const {data, status} = await axios({
+                method: 'POST',
+                data: loginData,
+                url: "/api/auth/login"
+            })
+            if (status === 200) {
+                authDispatch({type: "set_login", payload: true});
+                authDispatch({type: "set_user", payload: data?.foundUser});
+                authDispatch({type: "set_token", payload: data?.encodedToken});
+                navigate('/');
+                localStorage.setItem('token', data?.encodedToken)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    
     return (
         <>
-            <AuthContext.Provider>
+            <AuthContext.Provider value={{authState, userLogin}}>
                 {children}
             </AuthContext.Provider>
         </>
---
+++ src/reducer/authReducer.js
@@ -0,0 +1,12 @@
+export const authReducer = (state, action) => {
+    switch(action.type) {
+        case 'set_login':
+            return {...state, isLoggedIn: action.payload};
+        case  'set_user':
+            return {...state, user: action.payload};
+        case 'set_token':
+            return {...state, token: action.payload}
+        default:
+            return state;
+    }
+}
