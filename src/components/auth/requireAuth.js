2290faf6b2ee860015c685533360f42420065c5f
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-18 17:41:50 +0530

    feat: created require auth component

---
+++ src/components/auth/requireAuth.js
@@ -0,0 +1,8 @@
+import { Navigate, Outlet, useLocation } from "react-router";
+import { useAuth } from "../../context/authContext"
+
+export const RequireAuth = () => {
+    const {authState} = useAuth();
+    const location = useLocation();
+    return authState.isLoggedIn ? <Outlet /> : <Navigate to='/login' state={{from: location}} replace />
+}
