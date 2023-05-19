3ef7334198c6237f414a8093f948651ab4a2cad9
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-19 15:42:41 +0530

    added cart functionalities

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,7 +1,7 @@
 import { useCart } from '../../context/cartContext';
 import './cart.css';
 import { CartCard } from './cartCard';
-import { EmptyCart } from './emptyCart';
+import EmptyCart from './emptyCart';
 
 const Cart = () => {
     const {cart} = useCart();
@@ -10,29 +10,26 @@ const Cart = () => {
         totalPrice: 0,
         totalOriginalPrice: 0
     }
+
     return (
         <>
-            <div className="cart">
+            <div className='cart'>
                 <h1>Cart {cart.length > 0 && <span>({cart.length})</span>}</h1>
                 {cart.length > 0 ? <div>
                     <div>
-                        {cart.map(cart => {
-                            const {_id, brand, title, price, originalPrice, ratings, categoryName, qty} = cart
+                        {cart.map(item => {
+                            const { price, originalPrice,  qty} = item;
                             cartObject = {
                                 quantity: cartObject.quantity + Number(qty),
                                 totalPrice: cartObject.totalPrice + Number(price),
                                 totalOriginalPrice: cartObject.totalOriginalPrice + Number(originalPrice)
                             }
-                            
-                            // return (
-                            //     <CartCard />
-                            // )
+                            return <CartCard />
                         })}
                     </div>
-
                 </div> : <EmptyCart />}
-            </div>
-        </>
+            </div>            
+       </>
     )
 }  
 
