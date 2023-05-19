fca14cc0e72a35b991255a16be144ae42ee53825
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-19 12:34:47 +0530

    docs: update functions in cart context

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -1,11 +1,46 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import axios from "axios";
+import { useAuth } from "./authContext";
 
 const CartContext = createContext();
 
 export const CartProvider = ({children}) => {
+    const [cart, setCart] = useState([]);
+    const {token: encodedToken} = useAuth();
+
+    const getCartData = async() => {
+        try {
+            const {data, status} = await axios({
+                method: 'GET',
+                url: '/api/user/cart',
+                headers: {authorization: encodedToken}
+            });
+            if (status === 200) {
+                setCart(data?.cart)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const addCartData = async(cartData) => {
+        try {
+            const {data, status} = await axios({
+                method: "POST",
+                url: "/api/user/cart",
+                data: {product: cartData},
+                headers: {authorization: encodedToken}
+            })
+            if (status === 201) {
+                setCart(data?.cart)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
     return (
         <>
-            <CartContext.Provider>
+            <CartContext.Provider value={{cart, getCartData, addCartData}}>
                 {children}
             </CartContext.Provider>
         </>
