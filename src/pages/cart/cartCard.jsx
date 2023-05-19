b67ef59d9bf892e70bd9aac0d7ca4db335db9b18
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-19 16:40:42 +0530

    update cart card class

--- src/pages/cart/cartCard.jsx
+++ src/pages/cart/cartCard.jsx
@@ -1,13 +1,15 @@
+import './cart.css'
+
 export const CartCard = ({image, brand, title, quantity, price}) => {
     return (
         <>
-            <div>
+            <div className='cart-card'>
                 <img src={image} alt={title} />
                 <div className="title">
                     <h3>{brand}</h3>
                     <p>{title}</p>
                 </div>
-                <div className="cart-quanity">
+                <div className="cart-quantity">
                     <div className="quantity-select">
                         <i disabled={quantity===1 ? true : false} class="fa-solid fa-minus fa-xs"></i>{quantity}<i class="fa-solid fa-plus fa-xs"></i> 
                     </div>
