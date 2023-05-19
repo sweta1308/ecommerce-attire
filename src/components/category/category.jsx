f431faa47edd546bfb9b97a3f4c9d3c71c66a561
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-19 09:38:16 +0530

    feat: update category filter

--- src/components/category/category.jsx
+++ src/components/category/category.jsx
@@ -1,18 +1,23 @@
 import './category.css';
 import { useProducts } from '../../context/productContext';
+import { useFilters } from '../../context/filterContext';
+import { useNavigate } from 'react-router';
 
 export const Category = () => {
     const {productState} = useProducts();
+    const {filterDispatch} = useFilters();
+    const navigate = useNavigate();
     return (
         <>
             <h2>Currated Picks ✨</h2>
             <div className="category">
-                {productState?.categoriesData?.map(category => (
-                    <div>
+                {productState?.categoriesData.length !==0 && (productState?.categoriesData?.map(category => (
+                    <div onClick={() => {filterDispatch({type: 'filter_by_category', payload: category.categoryName});
+                    navigate('/products')}}>
                         <img src={category.image} alt={category.categoryName} />
-                        <a href='/'>{category.categoryName} <i class="fa-solid fa-arrow-right"></i></a>
+                        <p>{category.categoryName} <i class="fa-solid fa-arrow-right"></i></p>
                     </div>
-                ))}
+                )))}
             </div>
         </>
     )
--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -1,45 +1,45 @@
 export const filterReducer = (state, action) => {
-    switch(action.type) {
-        case "filter_by_category":
-            return state.category.includes(action.payload)
-            ? {
-                ...state,
-                category: [...state.category].filter(
-                  (item) => item !== action.payload
-                )
-              }
-            : {
-                ...state,
-                category: [...state.category, action.payload]
-              };
+  switch(action.type) {
+    case "filter_by_category":
+      return state.category.includes(action.payload)
+        ? {
+            ...state,
+            category: [...state.category].filter(
+              (item) => item !== action.payload
+            )
+          }
+        : {
+            ...state,
+            category: [...state.category, action.payload]
+          };
 
-        case "filter_by_brands":
-            return state.brands.includes(action.payload)
-            ? {
-                ...state,
-                brands: [...state.brands].filter(
-                  (item) => item !== action.payload
-                )
-              }
-            : {
-                ...state,
-                brands: [...state.brands, action.payload]
-              };
+    case "filter_by_brands":
+      return state.brands.includes(action.payload)
+        ? {
+            ...state,
+            brands: [...state.brands].filter(
+              (item) => item !== action.payload
+            )
+          }
+        : {
+            ...state,
+            brands: [...state.brands, action.payload]
+          };
         
-        case 'filter_by_rating':
-            return {...state, rating: action.payload}
+    case 'filter_by_rating':
+      return {...state, rating: action.payload}
 
-        case 'filter_by_sort': 
-            return {...state, sort: action.payload}
+    case 'filter_by_sort': 
+      return {...state, sort: action.payload}
 
-        case "clear_filters":
-            return {
-                category: [], 
-                brands: [],
-                rating: 5,
-                sort: 'featured'
-            }
-        default:
-            return state
-    }
+    case "clear_filters":
+      return {
+        category: [], 
+        brands: [],
+        rating: 5,
+        sort: 'featured'
+      }
+    default:
+      return state
+  }
 }
