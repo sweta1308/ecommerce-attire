66adb2dd872adb99ffdc09f3d5f49de555cd4744
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-19 15:42:55 +0530

    update heading

--- src/pages/cart/cartCard.jsx
+++ src/pages/cart/cartCard.jsx
@@ -2,7 +2,8 @@ export const CartCard = ({image, title}) => {
     return (
         <>
             <div>
-                <img src={image} alt={title} />
+                {/* <img src={image} alt={title} /> */}
+                <h1>Hello</h1>
             </div>
         </>
     )
