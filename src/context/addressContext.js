d436638423e3a0782c5fd8c23f841c9e44d83fcc
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-20 20:38:22 +0530

    update checkout reducer

--- src/context/addressContext.js
+++ src/context/addressContext.js
@@ -1,70 +1,98 @@
 import axios from "axios";
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
+import { checkoutReducer } from "../reducer/checkoutReducer";
 
 const AddressContext = createContext();
 
-export const AddressProvider = ({children}) => {
-    const [addressData, setAddressData] = useState([]);
-    const token = localStorage.getItem("token");
+export const AddressProvider = ({ children }) => {
+  const [addressData, setAddressData] = useState([]);
+  const token = localStorage.getItem("token");
 
-    const getAddressData = async() => {
-        try {
-            const {status, data} = await axios({
-                method: 'GET',
-                url: '/api/user/addresses',
-                headers: {authorization: token}
-            })
-            if (status === 200) {
-                setAddressData(data?.address)
-            }
-        } catch (e) {
-            console.log(e)
-        }
+  const checkoutInitial = {
+    name: '',
+    street: '',
+    city: '',
+    state: '',
+    pincode: ''
+  };
+
+  const [checkoutState, checkoutDispatch] = useReducer(
+    checkoutReducer,
+    checkoutInitial
+  );
+
+  const getAddressData = async () => {
+    try {
+      const { status, data } = await axios({
+        method: "GET",
+        url: "/api/user/addresses",
+        headers: { authorization: token },
+      });
+      if (status === 200) {
+        setAddressData(data?.address);
+      }
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    const addAddressData = async(addressData) => {
-        try {
-            const {data, status} = await axios({
-                method: "POST",
-                url: '/api/user/address',
-                data: {address: addressData},
-                headers: {authorization: token}
-            })
-            if (status === 201) {
-                setAddressData(data?.address)
-            }
-        } catch (e) {
-            console.log(e)
-        }
+  const addAddressData = async (addressData) => {
+    try {
+      const { data, status } = await axios({
+        method: "POST",
+        url: "/api/user/address",
+        data: { address: addressData },
+        headers: { authorization: token },
+      });
+      if (status === 201) {
+        setAddressData(data?.address);
+      }
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    const removeAddressData = async (dataId) => {
-        try {
-            const {data, status} = await axios({
-                method: "DELETE",
-                url: `/api/user/address/${dataId}`,
-                headers: {authorization: token}
-            })
-            if (status === 200) {
-                setAddressData(data?.address)
-            }
-        } catch (e) {
-            console.log(e)
-        }
+  const removeAddressData = async (dataId) => {
+    try {
+      const { data, status } = await axios({
+        method: "DELETE",
+        url: `/api/user/address/${dataId}`,
+        headers: { authorization: token },
+      });
+      if (status === 200) {
+        setAddressData(data?.address);
+      }
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    useEffect(() => {
-        getAddressData()
+  useEffect(() => {
+    getAddressData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+  }, []);
 
-    return (
-        <>
-            <AddressContext.Provider value={{addressData, addAddressData, removeAddressData}}>
-                {children}
-            </AddressContext.Provider>
-        </>
-    )
-}
+  return (
+    <>
+      <AddressContext.Provider
+        value={{
+          addressData,
+          addAddressData,
+          removeAddressData,
+          checkoutState,
+          checkoutDispatch,
+        }}
+      >
+        {children}
+      </AddressContext.Provider>
+    </>
+  );
+};
 
-export const useAddress = () => useContext(AddressContext)
+export const useAddress = () => useContext(AddressContext);
