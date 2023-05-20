a3bc95c3303be7887b2e4f633ae686c2c8c0669b
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-20 20:04:24 +0530

    docs: update address context

--- src/context/addressContext.js
+++ src/context/addressContext.js
@@ -1,11 +1,66 @@
-import { createContext, useContext } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const AddressContext = createContext();
 
 export const AddressProvider = ({children}) => {
+    const [addressData, setAddressData] = useState([]);
+    const token = localStorage.getItem("token");
+
+    const getAddressData = async() => {
+        try {
+            const {status, data} = await axios({
+                method: 'GET',
+                url: '/api/user/addresses',
+                headers: {authorization: token}
+            })
+            if (status === 200) {
+                setAddressData(data?.address)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const addAddressData = async(addressData) => {
+        try {
+            const {data, status} = await axios({
+                method: "POST",
+                url: '/api/user/address',
+                data: {address: addressData},
+                headers: {authorization: token}
+            })
+            if (status === 201) {
+                setAddressData(data?.address)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const removeAddressData = async (dataId) => {
+        try {
+            const {data, status} = await axios({
+                method: "DELETE",
+                url: `/api/user/address/${dataId}`,
+                headers: {authorization: token}
+            })
+            if (status === 200) {
+                setAddressData(data?.address)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        getAddressData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
         <>
-            <AddressContext.Provider>
+            <AddressContext.Provider value={{addressData, addAddressData, removeAddressData}}>
                 {children}
             </AddressContext.Provider>
         </>
