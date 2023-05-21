fc6fb0b1c10975488d25bda6570c0e4989f90293
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-21 11:32:47 +0530

    docs: update address form

--- src/pages/address/addressForm.jsx
+++ src/pages/address/addressForm.jsx
@@ -1,28 +1,37 @@
-import { useAddress } from '../../context/addressContext'
-import './address.css'
+import { useAddress } from "../../context/addressContext";
+import "./address.css";
 
-export const AddressForm  = () => {
-    const {addressData, setIsAddressCardVisible} = useAddress();
-    return (
-        <>
-            <div className="address-form">
-                <h2>Select Address</h2>
-                {addressData.map(data => {
-                    const {_id, name, street, city, state, pincode} = data
-                    return (
-                        <div key={_id} className='address-list'>
-                            <input type='radio' name='address' />
-                            <div className='address-data'>
-                                <h3>{name}</h3>
-                                <p>{street},</p>
-                                <p>{city}, {state}</p>
-                                <p>Pincode: {pincode}</p>
-                            </div>
-                        </div>
-                    )
-                })}
-                <button onClick={() => setIsAddressCardVisible(true)}>Add New Address</button>
+export const AddressForm = () => {
+  const { addressData, setIsAddressCardVisible, checkout, setCheckout } = useAddress();
+  return (
+    <>
+      <div className="address-form">
+        <h2>Select Address</h2>
+        {addressData.map((data) => {
+          const { _id, name, street, city, state, pincode } = data;
+          return (
+            <div key={_id} className="address-list">
+              <input
+                type="radio"
+                name="address"
+                checked={checkout && checkout._id === _id}
+                onChange={() => setCheckout(data)}
+              />
+              <div className="address-data">
+                <h3>{name}</h3>
+                <p>{street},</p>
+                <p>
+                  {city}, {state}
+                </p>
+                <p>Pincode: {pincode}</p>
+              </div>
             </div>
-        </>
-    )
-}
+          );
+        })}
+        <button onClick={() => setIsAddressCardVisible(true)}>
+          Add New Address
+        </button>
+      </div>
+    </>
+  );
+};
