53ad08238b071accd06db6288901b85a7b453929
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-21 11:32:58 +0530

    docs: update checkout card

--- src/pages/address/checkoutCard.jsx
+++ src/pages/address/checkoutCard.jsx
@@ -1,8 +1,10 @@
+import { useAddress } from "../../context/addressContext";
 import { useCart } from "../../context/cartContext";
 import './address.css'
 
 export const CheckoutCard = () => {
     const {cart} = useCart();
+    const {checkout} = useAddress();
     return (
         <>
             <div className="checkout-details">
@@ -25,6 +27,14 @@ export const CheckoutCard = () => {
                 })}
                 <hr />
                 <h4>Deliver to</h4>
+                <hr />
+                <p><strong>{checkout.name}</strong></p>
+                <div className="checkout-address">
+                    <p>{checkout.street}</p>
+                    <p>{checkout.city}, {checkout.state}</p>
+                    <p>Pincode: {checkout.pincode}</p>
+                </div>
+                
             </div>
             
 
