a59061f9263aadbf53c159f0fa8552ea5cab7993
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-26 20:29:17 +0530

    update add wishlist button in cart card

--- src/pages/cart/cartCard.jsx
+++ src/pages/cart/cartCard.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router";
+import { useAuth } from "../../context/authContext";
 import { useCart } from "../../context/cartContext";
+import { useWishlist } from "../../context/wishlistContext";
+import { isItemInWishlist } from "../../utils/isItemPresentInWishlist";
+import { toast } from "react-toastify";
 import "./cart.css";
 
 export const CartCard = ({ data, handleRemoveCart }) => {
   const { _id, image, brand, title, qty, price, originalPrice } = data;
   const { changeCartQuantity } = useCart();
+  const { authState } = useAuth();
+  const { wishlist, addWishlistData } = useWishlist();
+  const navigate = useNavigate();
   return (
     <>
       <div className="cart-card">
@@ -33,6 +41,26 @@ export const CartCard = ({ data, handleRemoveCart }) => {
         <div className="cart-price">
           <h3>Rs.{price}</h3>
           <p>Rs. {originalPrice}</p>
+          <button
+            className="wishlist-btn"
+            onClick={() => {
+              if (authState.isLoggedIn) {
+                if (isItemInWishlist(wishlist, _id)) {
+                  navigate("/wishlist");
+                } else {
+                  addWishlistData(data);
+                  toast.success("Added to wishlist!");
+                }
+              } else {
+                toast.warning("Please login to proceed");
+                navigate("/login");
+              }
+            }}
+          >
+            {isItemInWishlist(wishlist, _id)
+              ? "Go to Wishlist"
+              : "Add to Wishlist"}
+          </button>
         </div>
       </div>
     </>
