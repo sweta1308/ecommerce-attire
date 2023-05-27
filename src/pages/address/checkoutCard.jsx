5d0abc03b962d198b6bd75d1838a9d21e4acc169
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-27 23:33:32 +0530

    add price details

--- src/pages/address/checkoutCard.jsx
+++ src/pages/address/checkoutCard.jsx
@@ -2,14 +2,13 @@ import { useNavigate } from "react-router";
 import { useAddress } from "../../context/addressContext";
 import { useCart } from "../../context/cartContext";
 import "./address.css";
-import {toast} from 'react-toastify';
+import { toast } from "react-toastify";
 
 export const CheckoutCard = () => {
-  const { cart, removeCartData } = useCart();
-  let { cartObject } = useCart();
+  const { cart, removeCartData, priceDetails } = useCart();
   const { checkout } = useAddress();
   const navigate = useNavigate();
-
+  console.log(priceDetails);
   return (
     <>
       <div className="checkout-details">
@@ -37,40 +36,23 @@ export const CheckoutCard = () => {
         <hr />
         <h4>Price Details</h4>
         <hr />
-        {cart.map((item) => {
-          const { _id, price, originalPrice, qty } = item;
-          cartObject = {
-            quantity: cartObject.quantity + Number(qty),
-            totalPrice: cartObject.totalPrice + Number(price) * Number(qty),
-            totalOriginalPrice:
-              cartObject.totalOriginalPrice +
-              Number(qty) * Number(originalPrice),
-          };
-          return (
-            <>
-              <div key={_id}>
-                <ul className="checkout-price">
-                  <li>
-                    <p>Subtotal</p>
-                    <h4>Rs. {cartObject.totalOriginalPrice}</h4>
-                  </li>
-                  <li>
-                    <p>Discount</p>
-                    <h4>
-                      - Rs.{" "}
-                      {cartObject.totalOriginalPrice - cartObject.totalPrice}
-                    </h4>
-                  </li>
-                  <hr />
-                  <li>
-                    <p>Grand Total</p>
-                    <h4>Rs. {cartObject.totalPrice}</h4>
-                  </li>
-                </ul>
-              </div>
-            </>
-          );
-        })}
+        <ul className="checkout-price">
+          <li>
+            <p>Subtotal</p>
+            <h4>Rs. {priceDetails.totalOriginalPrice}</h4>
+          </li>
+          <li>
+            <p>Discount</p>
+            <h4>
+              - Rs. {priceDetails.totalOriginalPrice - priceDetails.totalPrice}
+            </h4>
+          </li>
+          <hr />
+          <li>
+            <p>Grand Total</p>
+            <h4>Rs. {priceDetails.totalPrice}</h4>
+          </li>
+        </ul>
         <hr />
         <h4>Deliver to</h4>
         <hr />
@@ -89,11 +71,18 @@ export const CheckoutCard = () => {
           </div>
         )}
 
-        <button disabled={Object.values(checkout)[0].length === 0 || cart.length === 0} onClick={() => {
-          navigate('/orderSummary');
-          cart.map(item => removeCartData(item._id));
-          toast.success("Order Placed!")
-        }}>Place Order</button>
+        <button
+          disabled={
+            Object.values(checkout)[0].length === 0 || cart.length === 0
+          }
+          onClick={() => {
+            navigate("/orderSummary");
+            cart.map((item) => removeCartData(item._id));
+            toast.success("Order Placed!");
+          }}
+        >
+          Place Order
+        </button>
       </div>
     </>
   );
