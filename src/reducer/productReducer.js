f988afc2c85dce760c80f2b3270c849df439d60a
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-28 21:51:12 +0530

    add details loader case

--- src/reducer/productReducer.js
+++ src/reducer/productReducer.js
@@ -4,11 +4,12 @@ export const productReducer = (state, action) => {
       return { ...state, isProductLoading: action.payload };
     case "categories_loading":
       return { ...state, isCategoryLoading: action.payload };
+    case "detail_loading":
+      return {...state, isDetailLoading: action.payload};
     case "set_products":
       return { ...state, productData: action.payload };
     case "set_product":
       return { ...state, product: action.payload };
-
     case "set_category":
       return { ...state, categoriesData: action.payload };
     default:
