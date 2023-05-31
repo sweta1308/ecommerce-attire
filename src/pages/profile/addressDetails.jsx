2d557c9e34c81e16fdc4e7885b96d46823843f60
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-31 18:11:41 +0530

    feat: add data

--- src/pages/profile/addressDetails.jsx
+++ src/pages/profile/addressDetails.jsx
@@ -1,3 +1,4 @@
+import { AddressCard } from "../../components/address/addressCard";
 import { useAddress } from "../../context/addressContext";
 import "./profile.css";
 import { NavLink } from "react-router-dom";
@@ -7,35 +8,67 @@ export const AddressDetails = () => {
     color: isActive ? "var(--primary-color)" : "black",
     borderBottom: isActive ? "1px solid var(--primary-color)" : "none",
   });
-  const { addressData, removeAddressData, setIsAddressCardVisible } = useAddress();
+  const {
+    addressData,
+    removeAddressData,
+    setIsAddressCardVisible,
+    isAddressCardVisible,
+    setCheckout,
+  } = useAddress();
   return (
-    <>
-      <h1>Account</h1>
-      <div className="profile">
-        <div className="links">
-          <NavLink style={getStyles} to="/profile">
-            Profile
-          </NavLink>
-          <NavLink style={getStyles} to="/address-details">
-            Address
-          </NavLink>
-        </div>
-        <button style={{cursor: 'pointer'}} onClick={() => setIsAddressCardVisible(true)}>ADD NEW ADDRESS</button>
-        {addressData.length === 0 && <h2>No addresses added.</h2>}
-        {addressData.map(data => {
-            const {_id, name, street, city, state, pincode} = data;
+    <div>
+      <div style={{ filter: isAddressCardVisible ? "blur(10px)" : "" }}>
+        <h1>Account</h1>
+        <div className="profile">
+          <div className="links">
+            <NavLink style={getStyles} to="/profile">
+              Profile
+            </NavLink>
+            <NavLink style={getStyles} to="/address-details">
+              Address
+            </NavLink>
+          </div>
+          <button
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              setIsAddressCardVisible(true);
+              setCheckout({
+                name: "",
+                street: "",
+                city: "",
+                state: "",
+                pincode: "",
+              });
+            }}
+          >
+            ADD NEW ADDRESS
+          </button>
+          {addressData.length === 0 && <h2>No addresses added.</h2>}
+          {addressData.map((data) => {
+            const { _id, name, street, city, state, pincode } = data;
             return (
-                <div key={_id} className="address-container">
-                    <p>{name}</p>
-                    <p>{street}</p>
-                    <p>{city}, {state}</p>
-                    <p>{pincode}</p>
-                    <button onClick={() => setIsAddressCardVisible(true)}>Edit</button>
-                    <button onClick={() => removeAddressData(_id)}>Delete</button>
-                </div>
-            )
-        })}
+              <div key={_id} className="address-container">
+                <p>{name}</p>
+                <p>{street}</p>
+                <p>
+                  {city}, {state}
+                </p>
+                <p>{pincode}</p>
+                <button
+                  onClick={() => {
+                    setIsAddressCardVisible(true);
+                    setCheckout(data);
+                  }}
+                >
+                  Edit
+                </button>
+                <button onClick={() => removeAddressData(_id)}>Delete</button>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </>
+      {isAddressCardVisible && <AddressCard />}
+    </div>
   );
 };
