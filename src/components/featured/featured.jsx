9bb01baf7a338997053f12fce839d7d5ae54e307
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-31 11:33:13 +0530

    feat: add route for featured cards

--- src/components/featured/featured.jsx
+++ src/components/featured/featured.jsx
@@ -1,15 +1,17 @@
 import { featuredData } from "./featuredData";
 import { ProductCard } from "../product-card/productCard";
 import "./featured.css";
+import { useNavigate } from "react-router";
 
 export const Featured = () => {
+  const navigate = useNavigate();
   return (
     <>
       <h2>Products You May Like ✨</h2>
       <div className="featured">
         {featuredData.map((data) => {
           return (
-            <div key={data._id}>
+            <div key={data._id} onClick={() => navigate('/products')}>
               <ProductCard data={data} />
             </div>
           );
