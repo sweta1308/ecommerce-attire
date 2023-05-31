60c7e03d5b4a06be8177fac7c9a097965afbd5d9
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-05-31 11:33:54 +0530

    removed 0 rating

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -166,7 +166,6 @@ export const SideBar = ({ isFilterVisible }) => {
             }
           />
           <datalist id="numbers">
-            <option value="0"></option>
             <option value="1"></option>
             <option value="2"></option>
             <option value="3"></option>
