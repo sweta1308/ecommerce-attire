c85f1720bed6ecbb69c0ff60717cce24a762d3dc
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-02 15:38:49 +0530

    add featured Data

--- src/components/featured/featured.jsx
+++ src/components/featured/featured.jsx
@@ -5,15 +5,18 @@ import { useProducts } from "../../context/productContext";
 
 export const Featured = () => {
   const {productState} = useProducts();
-  const {productData} = productState
-  const featuredData = [productData[22], productData[18], productData[5], productData[29], productData[19]]
+  const {productData} = productState;
+  let featuredData = []
+  if (productData.length > 0) {
+    featuredData = [productData[22], productData[18], productData[5], productData[29], productData[19]]
+  }
   return (
     <>
       <h2>Products You May Like ✨</h2>
       <div className="featured">
-        {featuredData.map((data) => {
+        {featuredData?.map((data) => {
           return (
-            <div key={data._id}>
+            <div>
               <ProductCard data={data} />
             </div>
           );
