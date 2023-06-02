fc8368ffb453359971e0234b394405be6bec353a
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-02 15:43:30 +0530

    remove token condition

--- src/reducer/authReducer.js
+++ src/reducer/authReducer.js
@@ -2,8 +2,6 @@ export const authReducer = (state, action) => {
   switch (action.type) {
     case "set_loading":
       return {...state, isAuthLoading: action.payload};
-    case "set_login":
-      return { ...state, isLoggedIn: action.payload };
     case "set_user":
       return { ...state, user: action.payload };
     case "set_token":
