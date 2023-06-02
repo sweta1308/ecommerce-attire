a67d508a1f4c4a4ea361797028201d6e3a212024
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-06-02 15:38:30 +0530

    remove authState

--- src/components/auth/requireAuth.js
+++ src/components/auth/requireAuth.js
@@ -2,9 +2,9 @@ import { Navigate, Outlet, useLocation } from "react-router";
 import { useAuth } from "../../context/authContext";
 
 export const RequireAuth = () => {
-  const { authState } = useAuth();
+  const { token } = useAuth();
   const location = useLocation();
-  return authState.token ? (
+  return token ? (
     <Outlet />
   ) : (
     <Navigate to="/login" state={{ from: location }} replace />
