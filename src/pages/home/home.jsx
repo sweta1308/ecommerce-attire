d4b787223027e32392c020d879af1350c859ee67
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-08-09 23:25:38 +0530

    refactor: remove footer

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,15 +1,15 @@
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Brand } from "../../components/brand/brand";
 import { Featured } from "../../components/featured/featured";
 import { Category } from "../../components/category/category";
-import { header, logo } from "../../assets";
+import { header } from "../../assets";
 import ClipLoader from "react-spinners/ClipLoader";
 import "./home.css";
 import { useProducts } from "../../context/productContext";
+import { Footer } from "../../components/footer/footer";
 
 export const Home = () => {
-  const navigate = useNavigate();
-  const {productState} = useProducts();
+  const { productState } = useProducts();
   return (
     <>
       <div className="hero">
@@ -60,48 +60,19 @@ export const Home = () => {
             <p>We offer fast and free shipping for our loyal customers.</p>
           </div>
         </div>
-        {productState.isProductLoading ? <ClipLoader color={`var(--primary-color)`} size={40}/> : <Featured />}
+        {productState.isProductLoading ? (
+          <ClipLoader color={`var(--primary-color)`} size={40} />
+        ) : (
+          <Featured />
+        )}
 
-        {productState?.isCategoryLoading ? <ClipLoader color={`var(--primary-color)`} size={40}/> : <Category />}
-        
+        {productState?.isCategoryLoading ? (
+          <ClipLoader color={`var(--primary-color)`} size={40} />
+        ) : (
+          <Category />
+        )}
       </div>
-      <footer>
-        <div className="footer-div">
-          <div className="logo-footer">
-            <img src={logo} alt="" />
-            <p>
-              Specializes in providing high-quality, stylish productsfor your
-              wardrobe
-            </p>
-          </div>
-          <div className="contacts">
-            <div className="companies">
-              <h3>COMPANY</h3>
-              <p onClick={() => navigate("/")}>About</p>
-              <p onClick={() => navigate("/")}>Terms of Use</p>
-              <p onClick={() => navigate("/")}>Policies</p>
-            </div>
-
-            <div className="socials">
-              <h3>CONTACTS: </h3>
-              <a href="https://github.com/sweta1308" target="_blank" rel="noreferrer">
-                <i class="fa-brands fa-github fa-lg"></i> Github
-              </a>
-              <a href="https://twitter.com/AgarwallaSweta" target="_blank" rel="noreferrer">
-                <i class="fa-brands fa-twitter fa-lg"></i> Twitter
-              </a>
-              <a href="https://www.linkedin.com/in/sweta-agarwalla-45aa2324a/" target="_blank" rel="noreferrer">
-                <i class="fa-brands fa-linkedin fa-lg"></i> LinkedIn
-              </a>
-            </div>
-          </div>
-        </div>
-        <hr />
-        <p>
-          Copyright <i class="fa-regular fa-copyright"></i>2023 Attire. All
-          rights reserved.
-        </p>
-      </footer>
+      <Footer />
     </>
   );
 };
